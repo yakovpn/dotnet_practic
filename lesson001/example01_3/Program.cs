439b297879cbddc78d70cb6502ea265b23e763ae
Iakov <project3@panargin.ru>
2022-11-21 22:20:52 +0300

    example01_3

--- lesson001/example01_3/Program.cs
+++ lesson001/example01_3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число:");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+int negN = - N;
+
+while (negN < N) {
+    Console.Write($"{negN} ");
+    negN +=1;
+}

000f00f5d6d6423ab72e69bba4861cdd32d9fc05
Iakov <project3@panargin.ru>
2022-11-21 21:33:26 +0300

    example01_0

--- lesson001/example01_0/Program.cs
+++ lesson001/example01_0/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Квадрат этого числа = " + (a * a).ToString());

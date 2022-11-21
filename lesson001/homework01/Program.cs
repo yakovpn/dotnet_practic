67cee277d8f9f15422ad5607f5d878e1f0d89c29
Iakov <project3@panargin.ru>
2022-11-21 23:20:44 +0300

    homeWork

--- lesson001/homework01/Program.cs
+++ lesson001/homework01/Program.cs
@@ -4,13 +4,15 @@ int a = s == null ? 0 : int.Parse(s);
 Console.Write("Ведите второе число:");
 s = Console.ReadLine();
 int b = s == null ? 0 : int.Parse(s);
-if ((b * b) == a)
+if (a > b)
 {
-    Console.WriteLine("Первое число является квадратом второго");
+    Console.WriteLine($"Число {a} больше числа {b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Число {a} меньше числа {b}");
 }
 else
 {
-    Console.WriteLine("Первое число не является квадратом второго");
+    Console.WriteLine($"Число {a} равно числу {b}");
 }
-
-

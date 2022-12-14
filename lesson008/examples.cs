762c6fb7054b72d4da0ed1268d9c81ab718b5845
Iakov <project3@panargin.ru>
2022-12-14 19:17:09 +0300

    lesson008 prepare

---
+++ lesson008/examples.cs
@@ -0,0 +1,16 @@
+using DotNetPractic;
+public partial class lesson008
+{
+    public void exampleBody1()
+    {
+    }
+    public void exampleBody2()
+    {
+    }
+    public void exampleBody3()
+    {
+    }
+    public void exampleBody4()
+    {
+    }
+}

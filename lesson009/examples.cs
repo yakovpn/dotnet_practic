1a820d6c04a2e114bef4ecc769b9ef57be9f9098
Iakov <project3@panargin.ru>
2022-12-19 19:54:53 +0300

    lesson009 prepare

---
+++ lesson009/examples.cs
@@ -0,0 +1,20 @@
+using DotNetPractic;
+public partial class lesson009
+{
+
+    public void exampleBody1()
+    {
+    }
+    public void exampleBody2()
+    {
+    }
+    public void exampleBody3()
+    {
+    }
+    public void exampleBody4()
+    {
+    }
+    public void exampleBody5()
+    {
+    }
+}
---
+++ lesson009/home_works.cs
@@ -0,0 +1,24 @@
+using DotNetPractic;
+public partial class lesson009
+{
+
+    public void homeWorkBody1()
+    {
+    }
+    public void homeWorkBody2()
+    {
+    }
+    public void homeWorkBody3()
+    {
+    }
+    private void print3dMatrixWithIndex(Array arr)
+    {
+    }
+
+    public void homeWorkBody4()
+    {
+    }
+    public void homeWorkBody5()
+    {
+    }
+}
